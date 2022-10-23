849eba5d4951cbde96cc27e3ee94672cdf7f483a
pimpit <72886465+Zayheight@users.noreply.github.com>
2022-10-23 19:40:13 +0700

    update

--- lab12no1.php
+++ lab12no1.php
@@ -1,27 +1,31 @@
 <html><head><meta charset="utf-8">
 <script>
-var httpRequest;
-function send() {
-httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = showResult;
-var a = document.getElementById("a").value;
-var b = document.getElementById("b").value;
-var c = document.getElementById("c").value;
-var url= "add.php?a=" + a + "&b=" + b+ "&c=" + c;
-httpRequest.open("GET", url);
-httpRequest.send();
-}
-function showResult() {
-if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-document.getElementById("result").innerHTML = httpRequest.responseText;
-}
-}
+	var xmlHttp;
+
+    function send() {
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = showResult;
+
+        var a = document.getElementById("a1").value;
+        var b = document.getElementById("a2").value;
+        var c = document.getElementById("a3").value;
+        var url= "add.php?a=" + a*30 + "&b=" + b*70 +"&c="+c*30;
+       
+       xmlHttp.open("GET", url);
+       xmlHttp.send();   
+   }
+   function showResult() {
+       if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+           document.getElementById("result").innerHTML = xmlHttp.responseText;
+       }
+   }
 </script></head>
 <body>
-    <h1>บันทึกการขาย</h1>
-
-มะม่วง กก.ละ 30 บาท ขายได้ <input type="number" id="a" > กก.<br>
-ส้ม กก.ละ 70 บาท ขายได้<input type="number" id="b" > กก.<br>
-กล้วย หวีละ 30 บาท ขายได้<input type="number" id="c" onkeyup="send()" > หวี<br>
-<span id="result"></span>
+     
+     มะม่วง กก.ละ 30บาท ขายได้ = <input type="text" id="a1">กก.<br>
+     ส้ม กก.ละ 70บาท ขายได้ = <input type="text" id="a2">กก.<br>
+     กล้วย หวี.ละ 30บาท ขายได้ = <input type="text" id="a3" onkeyup="send()">หวี.<br>
+     
+     <span id="result"></span>
 </body></html>
+
